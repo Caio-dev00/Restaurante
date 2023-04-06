d6a8c4407a49eb288f17d89d024465704057f169
devMarques <caiohenrique1146@gmail.com>
2023-04-05 22:32:08 -0300

    adicionando botao pagina detail

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React, {useLayoutEffect} from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, ScrollView, Image } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
-import {Entypo} from '@expo/vector-icons'
+import {Entypo, AntDesign, Feather} from '@expo/vector-icons'
 
 export default function Detail(){
     const route = useRoute();
@@ -26,14 +26,68 @@ export default function Detail(){
     }, [navigation, data])
 
     return(
-        <View>
-            <Text>{data.name}</Text>
-        </View>
+        <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+            <Pressable>
+                <View style={styles.playIcon}>
+                    <AntDesign name="playcircleo" size={44} color="#FAFAFA"/>
+                </View>
+                <Image
+                    style={styles.image}
+                    source={{uri: data.cover}}
+                />
+            </Pressable>
+
+            <View style={styles.headerDetails}>
+                <View>
+                    <Text style={styles.title}>{data.name}</Text>
+                    <Text style={styles.ingredientesTxt}>Ingredientes ({data.total_ingredients})</Text>
+                </View>
+                <Pressable>
+                    <Feather name="share-2" size={24} color="#121212"/>
+                </Pressable>
+            </View>
+
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    constainer:{
-        backgroundColor: '#14e245',
-    }
+    container:{
+        backgroundColor: '#f3f9ff',
+        paddingTop: 14,
+        paddingHorizontal: 14
+    },
+    image: {
+        width: '100%',
+        height: 200,
+        borderRadius: 14,
+    },
+
+    playIcon: {
+        position: 'absolute',
+        zIndex: 9,
+        top: 0, left: 0, right: 0, bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    title: {
+        fontSize: 18,
+        marginTop: 14,
+        fontWeight: 'bold',
+        color: '#000',
+        marginBottom: 4
+    },
+
+    ingredientesTxt: {
+        marginBottom: 14,
+        fontSize: 16
+    },
+
+    headerDetails: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 14,
+    },
 })
